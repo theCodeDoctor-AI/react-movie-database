0f46624abf64e1bb511e3996597a0533614fc818
Mark <mranda10@my.centennialcollege.ca>
2022-01-11 13:35:32 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//Components
+import Home from './components/Home';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+
+// Styles 
+import { GlobalStyle } from './GlobalStyle';
+
+//const Star = () => React.createElement('div', null, 'This is a little star');
+
+const App = () => {
+  //return Star();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:movieId' element={<Movie />} /> 
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      {<GlobalStyle />}
+    </Router>
   );
 }
 
